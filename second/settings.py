ac2e2dcdb0451cf63de3f345dda08e6defe6e378
Iacob Berar <contactiacob@gmail.com>
2020-02-12 08:28:13 +0200

    Fix Static Files

--- second/settings.py
+++ second/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"accounts/static"),
-    'accounts/static/'
+    os.path.join(BASE_DIR, 'accounts/static'),
 ]
 LOGIN_REDIRECT_URL="/account/login.html"
